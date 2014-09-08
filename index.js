d0a655fec19fee455210a8c3b2f0c89cfe641666
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2014-09-09 09:45:01 +1000

    index.js: Add preprocessor logic

---
+++ index.js
@@ -0,0 +1,29 @@
+var util = require('util');
+
+
+var DECLARATION = 'window.__env__ = window.__env__ || {};\n',
+    TEMPLATE = function(name) {
+      var tmpl = 'window.__env__[\'%s\'] = \'%s\';\n'
+      return util.format(tmpl, name, process.env[name]);
+    };
+
+var createEnvPreprocessor = function(args, config, logger, helper) {
+  var log = logger.create('preprocessor.env'),
+      names = config.envPreprocessor || [];
+  log.info('Publishing variables: ', names);
+
+  return function(content, file, done) {
+    var envContent = DECLARATION;
+
+    for (var i in names) {
+      envContent += TEMPLATE(names[i]);
+    }
+
+    done(envContent + '\n' + content);
+  };
+
+};
+
+module.exports = {
+  'preprocessor:env': ['factory', createEnvPreprocessor]
+};
