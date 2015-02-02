768fda04ad135dcfb97c6304aa9a5770cc95193c
Jonathan Sokolowski <jsok@expert360.com>
2015-02-03 10:19:44 +1100

    Replace quotes around env var value

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var util = require('util');
 var DECLARATION = 'window.__env__ = window.__env__ || {};\n',
     TEMPLATE = function(name) {
       if (typeof process.env[name] !== 'undefined') {
-        var tmpl = 'window.__env__[\'%s\'] = %s;\n';
+        var tmpl = 'window.__env__[\'%s\'] = \'%s\';\n';
         return util.format(tmpl, name, process.env[name]);
       } else {
         return '';
